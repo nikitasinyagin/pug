0fafcd60a46bb5f0aedd3ce0ae22033802b8f634
Nikita Sinyagin <mailto:fofanit@gmail.com>
2018-07-17 11:30:54 +0300

    test

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,23 +5,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Nikita Sinyagin (mailto:fofanit@gmail.com)
-* @version [#59058]
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Nikita Sinyagin (mailto:fofanit@gmail.com)
+ * @version [#59058]
+ * @since 0.1
+ */
 public class CalculateTest {
 	/**
-	* Test echo.
-	*/ 
+	 * Test echo.
+	 */
 	@Test
 	public void whenTakeNameThenTreeEchoPlusName() {
 		String input = "Nikita Sinyagin";
-		String expect = "Echo, echo, echo : Nikita Sinyagin"; 
+		String expect = "Echo, echo, echo : Nikita Sinyagin";
 		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
 	}
- 
+
 }
