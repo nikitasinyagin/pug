3a8ccd397118512980d444848f15bffe6259670c
Nikita Sinyagin <mailto:fofanit@gmail.com>
2018-07-21 21:08:22 +0300

    3.4. Расстояние между точками в системе координат [#188]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,47 @@
+package ru.job4j.condition;
+
+/**
+ * Класс для расчета расстояния между двумя точками.
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    /**
+     * Инициализация точки в системе координат.
+     * @param x координата по оси Х.
+     * @param y координата по оси Y.
+     */
+    public  Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Расчет расстояния между двумя точками.
+     * @param that координаты точуи b.
+     * @return возвращает результат расчета.
+     */
+    public double distanceTo(Point that) {
+        return Math.sqrt(
+                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+        );
+    }
+
+    /**
+     * Метод main выводит резльтаты расчета на экран.
+     * @param args
+     */
+    public static void main(String[] args) {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
+        // сделаем вызов метода
+        System.out.println("x1 = " + a.x);
+        System.out.println("y1 = " + a.y);
+        System.out.println("x2 = " + b.x);
+        System.out.println("y2 = " + b.y);
+
+        double result = a.distanceTo(b);
+        System.out.println("Расстояние между точками А и В : " + result);
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for condition task.
+ *
+ * @author Nikita Sinyagin (mailto:fofanit@gmail.com)
+ * @version [#59058]
+ * @since 0.1
+ */
+package ru.job4j.condition;
