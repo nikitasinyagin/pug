57547d2ccffd95a055ad2b841d689a50df516bb9
Nikita Sinyagin <mailto:fofanit@gmail.com>
2018-09-11 11:51:44 +0300

    6.2 Форматирование

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -6,15 +6,16 @@ package ru.job4j.array;
 public class Turn {
     /**
      * Метод turn принимает массив и возвращает его в зеркальном виде.
+     *
      * @param array исходный массив.
      * @return перевернуты массив.
      */
     public int[] turn(int[] array) {
-            for (int index = 0; index < array.length / 2; index++) {
-                int i = array[index];
-                array[index] = array[array.length - index - 1];
-                array[array.length - index - 1] = i;
-            }
+        for (int index = 0; index < array.length / 2; index++) {
+            int i = array[index];
+            array[index] = array[array.length - index - 1];
+            array[array.length - index - 1] = i;
+        }
         return array;
     }
 }
