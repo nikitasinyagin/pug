05e6e17883df32c1acd2b0aed764933b4e833fa8
Nikita Sinyagin <fofanit@gmail.com>
2018-11-26 01:16:23 +0300

    4. Используя шаблон проектирования - стратегию [#786]

---
+++ chapter_002/src/main/java/ru/job4j/shape/Triangle.java
@@ -0,0 +1,14 @@
+package ru.job4j.shape;
+
+public class Triangle implements Shape {
+
+    @Override
+    public String draw() {
+        StringBuilder pic = new StringBuilder();
+        pic.append("   +   \n");
+        pic.append("  +++  \n");
+        pic.append(" +++++ \n");
+        pic.append("+++++++\n");
+        return pic.toString();
+    }
+}
